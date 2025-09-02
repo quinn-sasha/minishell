0af2824418665c6a6a65afd2bc126948b69b2503
Yuri Ishikawa <yurishik@c2r6s3.42tokyo.jp>
2025-09-02 10:33:03 +0900

    strcmpにNULLチェックを追加

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: yurishik <yurishik@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 20:40:33 by yurishik          #+#    #+#             */
-/*   Updated: 2025/09/01 21:13:30 by yurishik         ###   ########.fr       */
+/*   Updated: 2025/09/02 10:00:58 by yurishik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
 
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (-1);
+	if (!s2)
+		return (1);
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
