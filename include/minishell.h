865f5f5a0c6394152de45854847ce01ad3cfe012
quinn-sasha <quinnsashakenta@gmail.com>
2025-09-03 13:45:11 +0900

    Added: t_command structure

---
+++ include/minishell.h
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include "../libft/libft.h"
+
+#define TRUE 1
+#define ERROR -1
+
+typedef struct s_word_list {
+  struct s_word_list *next;
+  char *word;
+} t_word_list;
+
+typedef struct s_command {
+  t_word_list *words;
+} t_command;
