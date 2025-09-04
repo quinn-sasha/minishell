43b289abc32a4bb48ec7de97f795b68df100685e
quinn-sasha <quinnsashakenta@gmail.com>
2025-09-04 20:41:42 +0900

    Fixed: find_path() ft_strjoin() の２つ目の引数が char 型になっていた

--- src/main.c
+++ src/main.c
@@ -87,7 +87,7 @@ char *find_path(char *command_name) {
   char **paths = ft_split(getenv("PATH"), ':');
   int i = 0;
   while (paths[i]) {
-    char *dir_path = ft_strjoin(paths[i], '/');
+    char *dir_path = ft_strjoin(paths[i], "/");
     char *path = ft_strjoin(dir_path, command_name);
     free(dir_path);
     if (access(path, F_OK) == FAILED) {
