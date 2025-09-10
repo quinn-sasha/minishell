fb2534062b333559130e989a0f503fa8968bd524
quinn-sasha <quinnsashakenta@gmail.com>
2025-09-10 13:22:03 +0900

    Fixed: memory leak

--- tests/tokenize/main.c
+++ tests/tokenize/main.c
@@ -6,21 +6,22 @@ int main(int argc, char *argv[]) {
     return FAILED;
   }
   char *input = argv[1];
-  int error_status;
-  t_token *tokens = tokenize(input, &error_status);
-
   if (!input || !*input) {
     printf("Input string is empty\n");
     return SUCCESS;
   }
+
+  int error_status;
+  t_token *token = tokenize(input, &error_status);
   if (error_status == UNCLOSED_QUOTE_STATUS) {
-    // free tokens;
+    free_token(token);
     ft_putendl_fd(UNCLOSED_QUOTE_ERROR, STDERR_FILENO);
     return FAILED;
   }
-  t_token *iter = tokens;
+  t_token *iter = token;
   while (!at_eof(iter)) {
     printf("word: %s\n", iter->word);
     iter = iter->next;
   }
+  free_token(token);
 }
