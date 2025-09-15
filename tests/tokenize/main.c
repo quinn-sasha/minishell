b5a20e0d2b08848eb1de50d04fdfc496a9e1f662
quinn-sasha <quinnsashakenta@gmail.com>
2025-09-15 22:19:09 +0900

    Added token test functions

--- tests/tokenize/main.c
+++ tests/tokenize/main.c
@@ -2,6 +2,10 @@
 
 int main(void) {
   test_simple_command();
+  test_empty_string();
+  test_blank();
+  test_pipe_and_redirect();
+  test_unclosed_quote();
 }
 
 void test_simple_command() {
@@ -19,6 +23,72 @@ void test_simple_command() {
     exit(EXIT_FAILURE);
   free_token(token);
 }
+
+void test_empty_string() {
+  printf("Test empty string: '' ...  ");
+  char *input = "";
+  t_expected_token expected[] = {
+    {TOKEN_EOF, NULL}
+  };
+  int status;
+  t_token *token = tokenize(input, &status);
+  int result = assert_tokens_equal(token, expected, sizeof(expected) / sizeof(t_expected_token));
+  if (result == false)
+    exit(EXIT_FAILURE);
+  free_token(token);
+}
+
+void test_blank() {
+  printf("Test empty string: '    \t' ...  ");
+  char *input = "    \t";
+  t_expected_token expected[] = {
+    {TOKEN_EOF, NULL}
+  };
+  int status;
+  t_token *token = tokenize(input, &status);
+  int result = assert_tokens_equal(token, expected, sizeof(expected) / sizeof(t_expected_token));
+  if (result == false)
+    exit(EXIT_FAILURE);
+  free_token(token);
+}
+
+void test_pipe_and_redirect() {
+  printf("Test pipe and redirect: '<< END echo 'abc' | cat > outfile.txt' ...  ");
+  char *input = "<< END echo 'abc' | cat > outfile.txt";
+  t_expected_token expected[] = {
+    {TOKEN_OPERATOR, "<<"},
+    {TOKEN_WORD, "END"},
+    {TOKEN_WORD, "echo"},
+    {TOKEN_WORD, "'abc'"},
+    {TOKEN_OPERATOR, "|"},
+    {TOKEN_WORD, "cat"},
+    {TOKEN_OPERATOR, ">"},
+    {TOKEN_WORD, "outfile.txt"},
+    {TOKEN_EOF, NULL}
+  };
+  int status;
+  t_token *token = tokenize(input, &status);
+  int result = assert_tokens_equal(token, expected, sizeof(expected) / sizeof(t_expected_token));
+  if (result == false)
+    exit(EXIT_FAILURE);
+  free_token(token);
+}
+
+void test_unclosed_quote() {
+  printf("Test: Unclosed Quote 'echo \"hello'... ");
+  char *input = "'echo \"hello";
+  int status;
+  t_token *token = tokenize(input, &status);
+  if (status == UNCLOSED_QUOTE_STATUS) {
+    printf("PASS\n");
+  }
+  else {
+    printf("FAIL: unclosed errro was not detected\n");
+  }
+  free_token(token);
+}
+
+void test_pipe_and_redirect();
 
 bool assert_tokens_equal(t_token *token, t_expected_token *expected_tokens, int size) {
   t_token *iter = token;
--- tests/tokenize/test_tokenize.h
+++ tests/tokenize/test_tokenize.h
@@ -7,3 +7,7 @@ typedef struct s_expected_token {
 
 bool assert_tokens_equal(t_token *token, t_expected_token *expected_tokens, int size);
 void test_simple_command();
+void test_empty_string();
+void test_blank();
+void test_pipe_and_redirect();
+void test_unclosed_quote();
