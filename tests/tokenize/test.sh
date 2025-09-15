1f74198c5270d0763c01745f4778e6f1fe810c9a
quinn-sasha <quinnsashakenta@gmail.com>
2025-09-15 21:04:33 +0900

    Deleted: shell script

--- tests/tokenize/test.sh
+++
@@ -1,8 +0,0 @@
-./test_tokenize "ls -l"
-./test_tokenize 'grep -nr "main" *'
-./test_tokenize "grep -nr 'main' *"
-./test_tokenize "echo 'abc' | cat > outfile.txt"
-./test_tokenize "echo helloworld >> out"
-./test_tokenize ""
-./test_tokenize "< Makefile tr a-z A-Z | cat | "
-./test_tokenize 'echo "hello '
