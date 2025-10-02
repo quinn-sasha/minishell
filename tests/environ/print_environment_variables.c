f13a6b6cf724a8933e4e1d03b904d400b7f2871d
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-10-02 17:25:05 +0900

    Added test: print environ

---
+++ tests/environ/print_environment_variables.c
@@ -0,0 +1,13 @@
+#include "minishell.h"
+
+int main() {
+  t_map *envmap = init_environment();
+  t_item *iter = envmap->head.next;
+  printf("(name: value)\n");
+  while (iter) {
+    printf("(%s, %s)\n", iter->name, iter->value);
+    iter = iter->next;
+  }
+  clean_environment(envmap);
+  printf("--- END ---");
+}
