38c465a98c258ddbc7d0b8424e1be794fcffafc3
quinn-sasha <quinnsashakenta@gmail.com>
2025-10-03 14:18:12 +0900

    Added: test_expand_shell_parameter.c

--- tests/expand/test_expand_shell_parameter.c
+++ tests/expand/test_expand_shell_parameter.c
@@ -2,34 +2,37 @@
 
 /*
 このコマンドがすでに実行されているという前提で、テストが行われます.
-export HELLO="hello world\n"
+export HELLO="hello world"
 */
 
 void assert_same_string(const char *s1, const char *s2) {
+  if (s1 == NULL && s2 == NULL)
+    return;
+  if (s1 == NULL)
+    exit(EXIT_FAILURE);
+  if (s2 == NULL)
+    exit(EXIT_FAILURE);
   assert(strcmp(s1, s2) == 0);
 }
 
 void test_simple_expansion(void) {
-  printf("Test 'echo $HELLO' ... ");
+  printf("Test '$HELLO' ... ");
   t_map *envmap = init_environment();
   int status;
-  char *input = "echo $HELLO";
+  char *input = "$HELLO";
   t_token *token = tokenize(input, &status);
   t_simple_command *command = NULL;
   parse(&command, token);
   expand(command, envmap);
-  printf("after expand");
 
   t_token *iter = command->arguments;
-  assert_same_string(iter->word, "echo");
-  iter = iter->next;
-  assert_same_string(iter->word, "hello world\n");
+  assert_same_string(iter->word, getenv("HELLO"));
   clean_command(&command);
   clean_environment(envmap);
   printf("PASS\n");
 }
 
-int main(void) {
+int main() {
   test_append_character();
   test_simple_expansion();
 }
