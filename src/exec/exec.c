e1e4045911ad6e2fece9c37451e567613041fa85
Yuri Ishikawa <yurishik@c2r6s3.42tokyo.jp>
2025-10-06 14:40:35 +0900

    validate_accessをふんわり実装, その他こまかな修正

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -1,3 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurishik <yurishik@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 12:52:45 by yurishik          #+#    #+#             */
+/*   Updated: 2025/10/06 14:40:04 by yurishik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	error_and_exit(const char *cmd, const char *message, int status)
+{
+	prefix_error();
+	ft_dprintf(STDERR_FILENO, "%s: ", cmd);
+	if (errno != 0)
+		ft_dprintf(STDERR_FILENO, "%s\n", strerror(errno));
+	else
+		ft_dprintf(STDERR_FILENO, "%s\n", message);
+	exit(status);
+}
+
 static char	*path_join(const char *dir_path, const char *cmd)
 {
 	size_t	dir_len;
@@ -30,7 +53,7 @@ char	*search_path_mode(const char *filename, int mode)
 	while (paths[i])
 	{
 		full_cmd_path = path_join(paths[i], filename);
-		if (access(full_cmd_path, mode) == 0)
+		if (access(full_cmd_path, mode) == SUCCESS)
 		{
 			free_array(paths);
 			return (full_cmd_path);
@@ -55,12 +78,10 @@ char	*search_path(const char *filename)
 	return (path);
 }
 
-// t_tokenのargumentsをchar**にする
-char	**tokens_to_argv(t_token *token)
+int	count_token(t_token *token)
 {
-	char	**argv;
+	int		i;
 	t_token	*current;
-	size_t	i;
 
 	i = 0;
 	current = token;
@@ -69,6 +90,17 @@ char	**tokens_to_argv(t_token *token)
 		i++;
 		current = current->next;
 	}
+	return (i);
+}
+
+// t_tokenのargumentsをchar**にする
+char	**tokens_to_argv(t_token *token)
+{
+	char	**argv;
+	t_token	*current;
+	size_t	i;
+
+	i = count_token(token);
 	argv = xcalloc(i + 1, sizeof(char *));
 	i = 0;
 	current = token;
@@ -81,23 +113,65 @@ char	**tokens_to_argv(t_token *token)
 	return (argv);
 }
 
+/*
+void	do_redirect(t_redirect *redirect)
+{
+	if (redirect == NULL)
+		return ;
+	if (is_redirect(redirect))
+	{
+		// stashfdに当たる部分が必要ということ？ちょい混乱中なので後回し
+	}
+	else
+		assert_error("redirect");
+	do_redirect(redirect->next);
+}
+*/
+
+void	validate_access(char *path, char *cmd)
+{
+	struct stat	st;
+
+	if (path == NULL)
+		error_and_exit(cmd, COMMAND_NOT_FOUND_ERROR, COMMAND_NOT_FOUND_STATUS);
+	if (access(path, F_OK) < 0)
+	{
+		if (errno == ENOENT)
+			error_and_exit(cmd, "No such file or directory",
+				COMMAND_NOT_FOUND_STATUS);
+		else
+			error_and_exit(cmd, COMMAND_NOT_FOUND_ERROR,
+				COMMAND_NOT_FOUND_STATUS);
+	}
+	if (stat(path, &st) < 0)
+		fatal_error("fstat");
+	if (S_ISDIR(st.st_mode))
+		error_and_exit(cmd, "is a directory", PERMISSION_DENIED_STATUS);
+	if (access(path, X_OK) < 0)
+		error_and_exit(cmd, PERMISSION_DENIED_ERROR, PERMISSION_DENIED_STATUS);
+}
+
 // コマンド構造体を元にbuiltinではない関数を実行する
-void exec_nonbuiltin(t_simple_command *command)
+void	exec_nonbuiltin(t_simple_command *command)
 {
 	char	**argv;
 	char	*path;
+
 	argv = tokens_to_argv(command->arguments);
 	path = argv[0];
-	// TODO
+	if (ft_strchr(path, '/') == -1)
+		path = search_path(path);
+	validate_access(path, argv[0]);
+	// do_redirect(command->redirect);
 }
 
 // return last_status
 int	wait_pipe(pid_t last_pid)
 {
-	int	last_status;
-	int	wait_status;
-	pid_t wait_pid;
-	
+	int		last_status;
+	int		wait_status;
+	pid_t	wait_pid;
+
 	while (1)
 	{
 		wait_pid = wait(&wait_status);
@@ -108,14 +182,14 @@ int	wait_pipe(pid_t last_pid)
 			else
 				last_status = WEXITSTATUS(wait_status);
 		}
-		else if (wait_pid < 0) // error
+		else if (wait_pid < 0)
 		{
 			if (errno == ECHILD)
-				break;
+				break ;
 			if (errno == EINTR)
-				continue;
+				continue ;
 			else
-				fatal_error("wait"); // TODO
+				fatal_error("wait");
 		}
 	}
 	return (last_status);
