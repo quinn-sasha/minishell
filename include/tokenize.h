a95e4a9c87d75ce9dd16274aaabe9fb002365b1a
quinn-sasha <quinnsashakenta@gmail.com>
2025-10-15 17:39:28 +0900

    Added is_quote_marker()

--- include/tokenize.h
+++ include/tokenize.h
@@ -38,6 +38,7 @@ void append_token(t_token **token, t_token *element);
 void insert_next_token(t_token *token, t_token *new_token);
 // tokenize.c
 int is_blank(int c);
+bool is_quote_marker(int c);
 bool is_quote(int c);
 int is_metacharacter(int c);
 t_token *consume_operator(char **input_to_advance, char *input);
