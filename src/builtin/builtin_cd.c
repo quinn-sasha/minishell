b51abc23191f845d8bfeb3e6b4429bf09e82693e
Yuri Ishikawa <yurishik@c2r6s3.42tokyo.jp>
2025-10-19 11:09:27 +0900

    builtin_cdで、ディレクトリがないときのエラーメッセージを修正

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: yurishik <yurishik@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:55:49 by yurishik          #+#    #+#             */
-/*   Updated: 2025/10/15 19:22:11 by yurishik         ###   ########.fr       */
+/*   Updated: 2025/10/19 11:06:56 by yurishik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,7 +108,7 @@ int	builtin_cd(char **argv, t_map *envmap)
 		return (1);
 	if (chdir(path) < 0)
 	{
-		perror_wrapper("cd", NULL, path);
+		perror_wrapper("cd", "no such file or directory", path);
 		return (1);
 	}
 	new_pwd = resolve_pwd(pwd, path);
