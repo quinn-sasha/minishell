57750431291235b10f2c3773b79fea3d8a3ca4d2
Yuri Ishikawa <yurishik@c2r6s3.42tokyo.jp>
2025-10-19 14:41:56 +0900

    builtin_cd()修正

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: yurishik <yurishik@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:55:49 by yurishik          #+#    #+#             */
-/*   Updated: 2025/10/19 13:35:27 by yurishik         ###   ########.fr       */
+/*   Updated: 2025/10/19 14:31:03 by yurishik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,13 +102,12 @@ int	builtin_cd(char **argv, t_map *envmap)
 	char	*pwd;
 	char	path[PATH_MAX];
 	char	*new_pwd;
-	char	*cwd;
 
 	pwd = xgetenv(envmap, "PWD");
-	if (pwd == NULL)
-		cwd = getcwd(NULL, 0);
+	if (pwd != NULL)
+		map_set(envmap, join_str_separator("OLDPWD", pwd, '='));
 	else
-		cwd = xstrdup(pwd);
+		map_set(envmap, "OLDPWD");
 	if (set_path_cd(path, pwd, argv, envmap) == FAILED)
 		return (1);
 	if (chdir(path) < 0)
@@ -117,13 +116,7 @@ int	builtin_cd(char **argv, t_map *envmap)
 		return (1);
 	}
 	new_pwd = resolve_pwd(pwd, path);
-	if (pwd != NULL)
-		map_set(envmap, join_str_separator("PWD", new_pwd, '='));
-	if (argv[1] == NULL)
-		map_set(envmap, "OLDPWD");
-	else if (ft_strcmp(cwd, "") != 0)
-		map_set(envmap, join_str_separator("OLDPWD", cwd, '='));
+	map_set(envmap, join_str_separator("PWD", new_pwd, '='));
 	free(new_pwd);
-	free(cwd);
 	return (SUCCESS);
 }
