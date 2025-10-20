e0a6573d01b8c3a189664afaa68f1c791e0b65ed
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-10-20 18:12:10 +0900

    Fixed norm: more than 25 lines

--- src/parse/redirect.c
+++ src/parse/redirect.c
@@ -3,50 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yurishik <yurishik@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 09:20:01 by yurishik          #+#    #+#             */
-/*   Updated: 2025/10/20 14:38:51 by yurishik         ###   ########.fr       */
+/*   Updated: 2025/10/20 18:10:14 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 void	fill_redirect(t_redirect *redirect, t_redirect_kind r_kind,
-			t_token *token)
+		t_token *token)
 {
-	char	*filename;
-
 	redirect->redirect_kind = r_kind;
+	redirect->is_filename_quoted = token->next->is_quoted;
+	if (r_kind == r_reading_until)
+	{
+		redirect->here_doc_eof = xstrdup(token->next->word);
+		return ;
+	}
+	redirect->to.filename = xstrdup(token->next->word);
 	if (r_kind == r_input_direction)
 	{
 		redirect->from.fd = STDIN_FILENO;
-		filename = xstrdup(token->next->word);
-		redirect->is_filename_quoted = token->next->is_quoted;
-		redirect->to.filename = filename;
 		redirect->open_flags = O_RDONLY;
 		return ;
 	}
 	if (r_kind == r_output_direction)
 	{
 		redirect->from.fd = STDOUT_FILENO;
-		filename = xstrdup(token->next->word);
-		redirect->is_filename_quoted = token->next->is_quoted;
-		redirect->to.filename = filename;
 		redirect->open_flags = O_CREAT | O_TRUNC | O_WRONLY;
 		return ;
 	}
 	if (r_kind == r_appending_to)
 	{
 		redirect->from.fd = STDOUT_FILENO;
-		filename = xstrdup(token->next->word);
-		redirect->is_filename_quoted = token->next->is_quoted;
-		redirect->to.filename = filename;
 		redirect->open_flags = O_CREAT | O_APPEND | O_WRONLY;
-		return ;
 	}
-	redirect->here_doc_eof = xstrdup(token->next->word);
-	redirect->is_filename_quoted = token->next->is_quoted;
 }
 
 t_redirect	*new_redirect(t_token *token)
