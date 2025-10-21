8212a3cd05522cf304982e9e822ea75bddea88e6
Yuri Ishikawa <yurishik@c2r6s3.42tokyo.jp>
2025-10-21 13:39:34 +0900

    fix: builtin_unset

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: yurishik <yurishik@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:24:13 by yurishik          #+#    #+#             */
-/*   Updated: 2025/10/20 14:35:00 by yurishik         ###   ########.fr       */
+/*   Updated: 2025/10/21 13:37:33 by yurishik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ int	builtin_unset(char **argv, t_map *envmap)
 {
 	char	*name;
 	int		i;
-  int status;
 
 	if (envmap == NULL)
 		return (FAILED);
-	status = SUCCESS;
 	i = 1;
 	while (argv[i] != NULL)
 	{
@@ -28,12 +26,11 @@ int	builtin_unset(char **argv, t_map *envmap)
 		if (!is_identifier(name))
 		{
 			perror_wrapper("unset", name, "not a valid identifier");
-			status = 1;
 			i++;
 			continue ;
 		}
 		map_delete(envmap, name);
 		i++;
 	}
-	return (status);
+	return (SUCCESS);
 }
